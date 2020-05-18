cc1e32fcf1038b0040fdf2e7c50c4b0cb5deb6fa
nurda_ryspay <nurdaryspay@gmail.com>
2020-05-19 00:26:33 +0600

    feat: toolbar, bottom menu

--- app/src/main/java/com/ryspay/nurda/HomeActivity.kt
+++ app/src/main/java/com/ryspay/nurda/HomeActivity.kt
@@ -2,11 +2,21 @@ package com.ryspay.nurda
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottom_navigation_view.setTextVisibility(false)
+            .enableItemShiftingMode(false)
+            .enableShiftingMode(false)
+            .enableAnimation(false)
+            .setIconSize(29f,29f)
+
+        for(i in 0 until bottom_navigation_view.menu.size()){
+            bottom_navigation_view.setIconTintList(i, null)
+        }
     }
 }
