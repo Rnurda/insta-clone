4259f27eccc4df8e75242a8d22b7f2c0321a45a9
nurda_ryspay <nurdaryspay@gmail.com>
2020-05-21 16:11:59 +0600

    connect: Firebase

--- app/src/main/java/com/ryspay/nurda/HomeActivity.kt
+++ app/src/main/java/com/ryspay/nurda/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.ryspay.nurda
 
 import android.os.Bundle
 import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : BaseActivity(0) {
     private val TAG = "HomeActivity"
@@ -11,5 +12,15 @@ class HomeActivity : BaseActivity(0) {
         setContentView(R.layout.activity_home)
         setUpBottomNavigation()
         Log.d(TAG, "onCreate: ")
+
+        val auth = FirebaseAuth.getInstance()
+        auth.signInWithEmailAndPassword("admin@gmail.com","password")
+            .addOnCompleteListener {
+                if(it.isSuccessful){
+                    Log.d(TAG, "Signin sucesss")
+                }else{
+                    Log.d(TAG, "Signin failure", it.exception)
+                }
+            }
     }
 }
