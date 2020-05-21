0c1591b71eed43a993b6019ca90908d62ff7001e
nurda_ryspay <nurdaryspay@gmail.com>
2020-05-21 17:38:44 +0600

    create: LoginActivity

--- app/src/main/java/com/ryspay/nurda/HomeActivity.kt
+++ app/src/main/java/com/ryspay/nurda/HomeActivity.kt
@@ -1,11 +1,13 @@
 package com.ryspay.nurda
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : BaseActivity(0) {
     private val TAG = "HomeActivity"
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,14 +15,24 @@ class HomeActivity : BaseActivity(0) {
         setUpBottomNavigation()
         Log.d(TAG, "onCreate: ")
 
-        val auth = FirebaseAuth.getInstance()
-        auth.signInWithEmailAndPassword("admin@gmail.com","password")
+        mAuth = FirebaseAuth.getInstance()
+        mAuth.signOut()
+        /*auth.signInWithEmailAndPassword("admin@gmail.com","password")
             .addOnCompleteListener {
                 if(it.isSuccessful){
                     Log.d(TAG, "Signin sucesss")
                 }else{
                     Log.d(TAG, "Signin failure", it.exception)
                 }
-            }
+            }*/
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if(mAuth.currentUser == null){
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
     }
 }
