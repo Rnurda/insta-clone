8ebf9c422a65edce1359311a5929ca3289c85da0
nurda_ryspay <nurdaryspay@gmail.com>
2020-05-21 23:55:10 +0600

    polish: Login style, btn  animation

--- app/src/main/java/com/ryspay/nurda/LoginActivity.kt
+++ app/src/main/java/com/ryspay/nurda/LoginActivity.kt
@@ -2,16 +2,46 @@ package com.ryspay.nurda
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import android.view.View
+import androidx.core.widget.addTextChangedListener
+import kotlinx.android.synthetic.main.activity_login.*
+import net.yslibrary.android.keyboardvisibilityevent.KeyboardVisibilityEvent
+import net.yslibrary.android.keyboardvisibilityevent.KeyboardVisibilityEventListener
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), KeyboardVisibilityEventListener, TextWatcher {
     private val TAG = "LoginActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         Log.d(TAG, "onCreate: ")
 
-
+        KeyboardVisibilityEvent.setEventListener(this,this)
+        login_btn.isEnabled = false
+        email_input.addTextChangedListener(this)
+        password_input.addTextChangedListener(this)
     }
+
+    override fun onVisibilityChanged(isKeyboardOpen: Boolean) {
+        if(isKeyboardOpen){
+            create_account_text.visibility = View.GONE
+            scrollView.scrollTo(0, scrollView.bottom)
+
+        }else{
+            scrollView.scrollTo(0, scrollView.top)
+            create_account_text.visibility = View.VISIBLE
+        }
+    }
+
+    override fun afterTextChanged(p0: Editable?) {
+       login_btn.isEnabled = email_input.text.toString().isNotEmpty() &&
+               password_input.text.toString().isNotEmpty()
+    }
+
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
 }
