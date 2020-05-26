8e0e4af72eec229c4bd2cac973ff882336d963ee
Rnurda <rnurda97@gmail.com>
2020-05-26 19:58:55 +0600

    feat: feed-posts

--- app/src/main/java/com/ryspay/nurda/activities/HomeActivity.kt
+++ app/src/main/java/com/ryspay/nurda/activities/HomeActivity.kt
@@ -3,13 +3,23 @@ package com.ryspay.nurda.activities
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import com.ryspay.nurda.R
+import com.ryspay.nurda.utils.FirebaseHelper
+import com.ryspay.nurda.utils.GlideApp
+import com.ryspay.nurda.utils.ValueEventListenerAdapter
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.feed_item.view.*
 
 class HomeActivity : BaseActivity(0) {
     private val TAG = "HomeActivity"
-    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mFirebase: FirebaseHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,19 +27,8 @@ class HomeActivity : BaseActivity(0) {
         setUpBottomNavigation()
         Log.d(TAG, "onCreate: ")
 
-        mAuth = FirebaseAuth.getInstance()
-        /*auth.signInWithEmailAndPassword("admin@gmail.com","password")
-            .addOnCompleteListener {
-                if(it.isSuccessful){
-                    Log.d(TAG, "Signin sucesss")
-                }else{
-                    Log.d(TAG, "Signin failure", it.exception)
-                }
-            }*/
-        sign_out_text.setOnClickListener {
-            mAuth.signOut()
-        }
-        mAuth.addAuthStateListener {
+        mFirebase = FirebaseHelper(this)
+        mFirebase.auth.addAuthStateListener {
             if(it.currentUser == null){
                 startActivity(Intent(this, LoginActivity::class.java))
                 finish()
@@ -39,9 +38,38 @@ class HomeActivity : BaseActivity(0) {
 
     override fun onStart() {
         super.onStart()
-        if(mAuth.currentUser == null){
+        val currentUser = mFirebase.auth.currentUser
+        if(currentUser == null){
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
+        }else{
+            mFirebase.database.child("feed-posts").child(currentUser.uid)
+                .addValueEventListener(ValueEventListenerAdapter{
+                    val posts = it.children.map { it.getValue(FeedPost::class.java)!! }
+                    Log.d(TAG, "feedPosts: ${posts.joinToString("\n", "\n")} ")
+                    Log.d(TAG, "feedPosts: ${posts.first().timeStampsDate()} ")
+
+                    feed_recycler.adapter = FeedAdapter(posts)
+                    feed_recycler.layoutManager = LinearLayoutManager(this)
+                })
         }
+    }
+}
+class FeedAdapter(private val posts: List<FeedPost>) : RecyclerView.Adapter<FeedAdapter.ViewHolder>(){
+    class ViewHolder(val view: View) : RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.feed_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder:ViewHolder, position: Int) {
+        holder.view.post_image.loadImage(posts[position].image)
+    }
+
+    override fun getItemCount(): Int  = posts.size
+
+    private fun ImageView.loadImage(image: String){
+        GlideApp.with(this).load(image).centerCrop().into(this)
     }
 }
--- app/src/main/java/com/ryspay/nurda/activities/ShareActivity.kt
+++ app/src/main/java/com/ryspay/nurda/activities/ShareActivity.kt
@@ -3,17 +3,23 @@ package com.ryspay.nurda.activities
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.util.Log
+import com.google.firebase.database.ServerValue
 import com.ryspay.nurda.R
+import com.ryspay.nurda.models.User
 import com.ryspay.nurda.utils.CameraHelper
 import com.ryspay.nurda.utils.FirebaseHelper
 import com.ryspay.nurda.utils.GlideApp
+import com.ryspay.nurda.utils.ValueEventListenerAdapter
 import kotlinx.android.synthetic.main.activity_share.*
+import java.util.*
 
 class ShareActivity : BaseActivity(2) {
     private val TAG = "ShareActivity"
     private lateinit var mCamera: CameraHelper
     private lateinit var mFirebase: FirebaseHelper
+    private lateinit var mUser: User
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +29,10 @@ class ShareActivity : BaseActivity(2) {
         mFirebase = FirebaseHelper(this)
         mCamera = CameraHelper(this)
         mCamera.takeCameraPicture()
+
+        mFirebase.currentUserReference().addValueEventListener(ValueEventListenerAdapter{
+            mUser = it.getValue(User::class.java)!!
+        })
 
         back_image.setOnClickListener{finish()}
         share_text.setOnClickListener { share() }
@@ -51,11 +61,19 @@ class ShareActivity : BaseActivity(2) {
                         // add image to user images <- DB
                         val downloadTask = it.result!!.metadata!!.reference!!.downloadUrl
                         downloadTask.addOnSuccessListener { downloadUrl ->
-                            mFirebase.database.child("images").child(uid).push().setValue(downloadUrl.toString())
+                            val imageDownloadURL = downloadUrl.toString()
+                            mFirebase.database.child("images").child(uid).push().setValue(imageDownloadURL)
                                 .addOnCompleteListener {
                                     if(it.isSuccessful){
-                                        startActivity(Intent(this, ProfileActivity::class.java))
-                                        finish()
+                                        mFirebase.database.child("feed-posts").child(uid)
+                                            .push()
+                                            .setValue(mkFeedPost(uid, imageDownloadURL))
+                                            .addOnCompleteListener {
+                                                if(it.isSuccessful){
+                                                    startActivity(Intent(this, ProfileActivity::class.java))
+                                                    finish()
+                                                }
+                                            }
                                     }else{
                                         showToast(it.exception!!.message!!)
                                     }
@@ -68,6 +86,26 @@ class ShareActivity : BaseActivity(2) {
         }
     }
 
-
+    private fun mkFeedPost(
+        uid: String,
+        imageDownloadURL: String
+    ): FeedPost {
+        return FeedPost(
+            uid = uid,
+            username = mUser.username,
+            image = imageDownloadURL,
+            caption = caption_input.text.toString(),
+            photo = mUser.photo
+        )
+    }
 }
+
+data class FeedPost(val uid: String = "", val username: String = "", val image: String = "",
+                    val likesCount: Int = 0, val commentsCount: Int = 0, val caption: String = "",
+                    val comments: List<Comment> = emptyList(), val timeStamp: Any = ServerValue.TIMESTAMP,
+                    val photo: String? =null ){
+    fun timeStampsDate(): Date = Date(timeStamp as Long)
+}
+
+data class Comment(val uid:String, val username: String, val text: String)
 
